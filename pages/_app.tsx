0da307c1aa98fb37850c748e58261e4bd7993728
fbjarkes <fbjarkes>
2021-03-20 17:04:35 +0100

    Basic next-js setup

---
+++ pages/_app.tsx
@@ -0,0 +1,33 @@
+import ThemeProvider  from '../src/theme/ThemeProvider';
+import React, { useEffect, useState } from 'react'
+import { AppProps } from 'next/app'
+import Header from '../src/components/header'
+import Footer from '../src/components/footer'
+import styles from '../styles/App.module.css'
+
+
+const App = ({ Component, pageProps }: AppProps): React.ReactNode => {
+  // const [style, setStyle] = useState<React.CSSProperties>({ visibility: 'hidden' })
+  useEffect(() => {
+    const jssStyles = document.querySelector('#jss-server-side')
+    if (jssStyles) {
+      jssStyles.parentElement.removeChild(jssStyles)
+    }
+  }, [])
+
+  return (
+    <>
+      <div className={styles['app-container']}>
+        <ThemeProvider>
+          <div className={styles['content-container']}>
+            <Header />
+            <Component {...pageProps} />
+          </div>
+          <Footer />
+        </ThemeProvider>
+      </div>
+    </>
+  )
+}
+
+export default App;
