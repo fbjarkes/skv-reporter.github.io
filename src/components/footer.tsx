67d7d56de2858af14442008e70fdce91a9c960fd
fbjarkes <fbjarkes-github.q5706@aleeas.com>
2022-02-19 12:08:40 +0100

    Add more Footer metrics

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -6,25 +6,36 @@ import { calculateStats } from '../utils/helper';
 // import { TradeStats } from '../types/tradestats';
 import { TradesContext } from '../contexts/TradesContext';
 
+
 const Footer: React.VFC = () => {
     const theme = useTheme();
     const { state } = useContext(TradesContext);
     const stats = calculateStats(state.filteredTrades);
 
+    const boxMargin = theme.spacing(1, 1, 4, 1);
+
     return (
         <>
             <Box display="flex" justifyContent="center">
-                <Box display="flex" flexDirection="column" style={{ margin: theme.spacing(1, 1, 4, 1) }}>
+                <Box display="flex" flexDirection="column" style={{ margin: boxMargin }}>
+                    <Typography>Trades shown: 0 </Typography>
+                    <Typography>Total trades: 0 </Typography>
+                </Box>
+                <Box display="flex" flexDirection="column" style={{ margin: boxMargin }}>
                     <Typography>PF: {stats.profitFactor}</Typography>
                     <Typography>WinRate (%): {stats.winRate}</Typography>
+                    <Typography>Total profits: {stats.totalWin}</Typography>
+                    <Typography>Total losses: {stats.totalLoss}</Typography>
+                </Box>
+                <Box display="flex" flexDirection="column" style={{ margin: boxMargin }}>
+                    <Typography>Winners: {stats.winners}</Typography>
+                    <Typography>Losers: {stats.losers}</Typography>
                     <Typography>AvgWin: {stats.avgWin}</Typography>
                     <Typography>AvgLoss: {stats.avgLoss}</Typography>
                 </Box>
-                <Box display="flex" flexDirection="column" style={{ margin: theme.spacing(1, 1, 4, 1) }}>
-                    <Typography>Total profits: {stats.totalWin}</Typography>
-                    <Typography>Total losses: {stats.totalLoss}</Typography>
-                    <Typography>Winners: {stats.winners}</Typography>
-                    <Typography>Losers: {stats.losers}</Typography>
+                <Box display="flex" flexDirection="column" style={{ margin: boxMargin }}>
+                    <Typography>Avg. holding time winner : 0 </Typography>
+                    <Typography>Avg. holding time loser : 0 </Typography>
                 </Box>
             </Box>
         </>
